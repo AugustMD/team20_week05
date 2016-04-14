abdd2a249dca2cce6bbff8df0fcc113e62907bf1
ParkSungjin <ParkSungjin@ParkSungjinui-MacBook-Pro.local>
2016-04-15 04:38:47 +0900

    wefwef

--- src/test/java/teamtwenty/aid/AppTest.java
+++ src/test/java/teamtwenty/aid/AppTest.java
@@ -12,7 +12,7 @@ import teamtwenty.aid.InputClass;
 public class AppTest {
 	BillView bv;
 	Customer customer;
-	InputClass input;
+	InputClass input[]=null;
 	
 	/*@BeforeClass
 	public void initAppTest() {
@@ -22,10 +22,16 @@ public class AppTest {
 	
 	@Test
 	public void testSilver() {
-		input = new InputClass("Silver",1005,3);	
-		customer = new Customer(input);
+		input[0] = new InputClass("Silver",600,4);
+		input[1] = new InputClass("Gold",1010,3);
+		input[2] = new InputClass("silver",400,1);
+		input[3] = new InputClass("gold",900,1);
+		
+		
+		for(int i=0;i<4;i++){
+			customer = new Customer(input[i]);
 		//customer.setUsedInfo("Silver", 1004, 3);
-		bv = new BillView(customer,input);
-		 
+			bv = new BillView(customer,input[i]);
+		}
 	}
 }
